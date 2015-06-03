5fe1fc8d66ad8fab164cd1b9e29ed2bcdf0559e0
David Jenkins <david.nicholas.jenkins@gmail.com>
2015-06-03 16:10:16 -0400

    A lot of the code here is temporary, but doing a commit so I can merge the latest commit from Joshua.

--- app/src/main/java/com/zakidd/ideatap/ui/MainActivity.java
+++ app/src/main/java/com/zakidd/ideatap/ui/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
@@ -12,19 +14,47 @@ import com.zakidd.ideatap.R;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnItemClick;
+import butterknife.OnTextChanged;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Firebase ref = new Firebase("https://idea-tap.firebaseio.com");
+    Firebase fireRef;
+
     @InjectView(R.id.googleLoginButton) Button loginButton;
+    @InjectView(R.id.testData) EditText testData;
+    @InjectView(R.id.testDataView) TextView testDataView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         Firebase.setAndroidContext(this);
+
+        fireRef = new Firebase("https://idea-tap.firebaseio.com/");
+
+        setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
+    }
+
+    @OnItemClick(R.id.googleLoginButton)
+    public void loginWithGoogle(int positionOfClick) {
+        fireRef.authWithOAuthToken("google", "", new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+
+            }
+
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+
+            }
+        });
+    }
+
+    @OnTextChanged(R.id.testData)
+    public void testDataChanged(CharSequence changedText) {
+        fireRef.child("test_data").setValue(changedText);
     }
 
 }
